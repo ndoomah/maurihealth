a68627d502b60a96dacd85b5fa31443a05b548f3
ndoomah <nabiiha.doomah@umail.uom.ac.mu>
2018-12-31 16:09:07 +0400

    Update saveData.php

--- saveData.php
+++ saveData.php
@@ -4,7 +4,7 @@ $disease = $_POST['diseasetype'];
 $location = $_POST['location'];
 $date = $_POST['date'];
 
-$client = new MongoDB\Client('mongodb://fyp_admin:fyp_pwd@cluster0-shard-00-00-oov30.mongodb.net:27017,cluster0-shard-00-01-oov30.mongodb.net:27017,cluster0-shard-00-02-oov30.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true');
+$client = new MongoDB\Client('mongodb+srv://fyp_admin:fyp_pwd@cluster0-oov30.mongodb.net/test?retryWrites=true');
 
 $db = $client->test;
 $collection = $db->flu;
@@ -15,7 +15,7 @@ $datainput = array(
 	'date' => $date
 );
 
-$collection->save($datainput);
+$collection->insertOne($datainput);
 
 header('Location: /index.html');
 ?>
